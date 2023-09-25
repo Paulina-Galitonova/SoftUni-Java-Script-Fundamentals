a8b8c0f96145cf7ece4e782efc67937ba09fbc70
Paulina-Galitonova <145610504+Paulina-Galitonova@users.noreply.github.com>
2023-09-25 20:42:33 +0300

    07. Lower or Upper created and compleated

---
+++ JavaScipt Fundamentals/Exercise/02.Exercises Data Types and Variables/07.lowerOrUpper.js
@@ -0,0 +1,13 @@
+function lowerOrUpper(char){
+
+//Write a function that prints whether a given character is upper-case or lower-case.
+
+if (char.toUpperCase()===char){
+    console.log("upper-case");
+
+} else{
+    console.log("lower-case");  
+}
+
+}
+lowerOrUpper ('f');
