29900579d2370f984dc79b54a5573795022e2221
Paulina-Galitonova <145610504+Paulina-Galitonova@users.noreply.github.com>
2023-09-25 16:47:31 +0300

    04. Integer and Float -compleated

---
+++ JavaScipt Fundamentals/LAB/02 Lab Data Types and Variables/04.integerFloat.js
@@ -0,0 +1,22 @@
+function integerFloat(num1, num2, num3) {
+
+    let sum = num1 + num2 + num3;
+    let typeNum="";
+
+    if (sum % 1 ==0) {
+        typeNum = "Integer";
+
+    } else {
+        typeNum = "Float";
+    }
+
+
+    console.log(`${sum} - ${typeNum}`);
+
+
+
+
+}
+
+
+integerFloat(100, 200, 303);
