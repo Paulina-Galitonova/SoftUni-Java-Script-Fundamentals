835b272b8f9664c3c122570b0760290716eef2d8
Paulina-Galitonova <145610504+Paulina-Galitonova@users.noreply.github.com>
2023-09-25 19:47:09 +0300

    02.Chars to String compleated

---
+++ JavaScipt Fundamentals/Exercise/02.Exercises Data Types and Variables/02.charsToString.js
@@ -0,0 +1,12 @@
+function charsToString(par1, par2, par3) {
+
+    //Write a function, which receives 3 parameters. 
+    //Each parameter is a single character. Combine all the characters into one string and print it on the console.
+
+console.log(`${par1}${par2}${par3}`)
+}
+
+charsToString('%',
+'2',
+'o'
+);
