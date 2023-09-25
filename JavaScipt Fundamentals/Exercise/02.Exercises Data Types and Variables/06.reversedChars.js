de6e3114754485c2e8e017969b6a63795f1891ce
Paulina-Galitonova <145610504+Paulina-Galitonova@users.noreply.github.com>
2023-09-25 20:42:14 +0300

    Update 06.reversedChars.js

---
+++ JavaScipt Fundamentals/Exercise/02.Exercises Data Types and Variables/06.reversedChars.js
@@ -0,0 +1,12 @@
+function reversedChars (char1,char2,char3){
+
+//Write a program that takes 3 parameters (characters) and prints them in reversed order with a space between them.
+
+console.log(`${char3} ${char2} ${char1}`)
+
+}
+
+reversedChars('A',
+'B',
+'C'
+);
