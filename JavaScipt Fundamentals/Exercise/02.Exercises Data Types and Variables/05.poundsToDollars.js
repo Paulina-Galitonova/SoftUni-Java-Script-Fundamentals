1698bcc692f428ff18b19320d22e4433d4ca97ac
Paulina-Galitonova <145610504+Paulina-Galitonova@users.noreply.github.com>
2023-09-25 20:35:05 +0300

    poundsToDollars created and compleated

---
+++ JavaScipt Fundamentals/Exercise/02.Exercises Data Types and Variables/05.poundsToDollars.js
@@ -0,0 +1,9 @@
+function poundsToDollars(pounds) {
+
+//Write a function that converts British pounds to dollars formatted to the 3rd decimal point. 
+let dollars= pounds *1.31;
+
+console.log(dollars.toFixed(3));
+
+}
+poundsToDollars(39);
