662d9c410f7042f7238addb78bb20d87d1388e66
Paulina-Galitonova <145610504+Paulina-Galitonova@users.noreply.github.com>
2023-09-25 20:28:10 +0300

    03.Town Info created and compleated

---
+++ JavaScipt Fundamentals/Exercise/02.Exercises Data Types and Variables/03.townInfo.js
@@ -0,0 +1,13 @@
+function townInfo(town,population,area){
+//You will be given 3 parameters. 
+//The first parameter will be the name of the town (string), the second – the population (number), 
+//and the third the area (number). Print the result in the following format:
+
+
+console.log(`Town ${town} has population of ${population} and area ${area} square km.`)
+}
+
+townInfo('Sofia',
+1286383,
+492
+);
