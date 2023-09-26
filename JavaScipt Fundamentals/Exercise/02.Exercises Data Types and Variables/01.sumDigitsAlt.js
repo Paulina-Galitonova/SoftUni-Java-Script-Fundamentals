0568ee006078c0b653648c70d7aaeadf513df87b
Paulina-Galitonova <145610504+Paulina-Galitonova@users.noreply.github.com>
2023-09-26 17:33:45 +0300

    01.sumDigitsAlt - created alternate code

---
+++ JavaScipt Fundamentals/Exercise/02.Exercises Data Types and Variables/01.sumDigitsAlt.js
@@ -0,0 +1,22 @@
+function sumDigitsAlt(num){
+
+    let sum=0;
+
+    while (num !=0) {
+        let units = num % 10;
+        num -=units;
+        num /=10;
+
+        sum +=units;
+    }
+
+
+    console.log(sum);
+
+
+
+
+
+
+} 
+sumDigitsAlt(245678);
