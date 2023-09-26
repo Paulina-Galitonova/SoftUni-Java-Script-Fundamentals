9ca59f510d4edcfaaf9c42a9ab939689ee4cfaa5
Paulina-Galitonova <145610504+Paulina-Galitonova@users.noreply.github.com>
2023-09-26 17:02:17 +0300

    10. Spice Must Flow compleated

---
+++ JavaScipt Fundamentals/Exercise/02.Exercises Data Types and Variables/10.spiceMustFlow.js
@@ -0,0 +1,46 @@
+    function spiceMustFlow(startingYield ){
+
+    //Write a program that calculates the total amount of spice that can be extracted from a source. 
+    //The source has a starting yield, which indicates how much spice can be mined on the first day.
+    // After it has been mined for a day, the yield drops by 10, meaning on the second day it’ll produce 10 less spice than on the first,
+    // on the third day 10 less than on the second, and so on (see examples).
+    // A source is considered profitable only while its yield is at least 100 – when less than 100 spices are expected in a day, 
+    //abandon the source. 
+
+    //The mining crew consumes 26 spices every day at the end of their shift and an additional 26 after the mine has been exhausted. 
+    //Note that the workers cannot consume more spice than there is in storage. 
+    //When the operation is complete, print on the console on two separate lines
+    // how many days the mine has operated and the total amount of spice extracted. 
+
+    let day = 0;
+    let totalAmountSpice=0;
+
+
+
+    while (startingYield >= 100) {
+
+        totalAmountSpice += (startingYield-26);
+        
+        startingYield -=10;
+    
+        
+        day++;
+    
+        
+    }
+
+    totalAmountSpice -=26;
+
+    if (totalAmountSpice < 0) {
+
+        totalAmountSpice = 0;
+
+    }
+
+    console.log(day);
+    console.log(totalAmountSpice);
+
+
+    }
+
+spiceMustFlow(450);
