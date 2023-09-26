0aac4cfc60cc4b4266adb6a8216a526c2cbeaaa9
Paulina-Galitonova <145610504+Paulina-Galitonova@users.noreply.github.com>
2023-09-26 17:02:29 +0300

    test code created

---
+++ JavaScipt Fundamentals/Exercise/02.Exercises Data Types and Variables/test.js
@@ -0,0 +1,52 @@
+function demo(startingYield) {
+
+    let totalExtract = 0;
+
+    let days = 0;
+
+
+
+    while (startingYield >= 100) {
+
+
+
+        totalExtract += (startingYield - 26);
+
+        // if (startingYield >= 26) {
+
+        // }
+
+
+
+        startingYield -= 10;
+
+        days++;
+
+    }
+
+
+
+    // if (startingYield < 100) {
+
+    // totalExtract -= 26;
+
+    // }
+
+
+
+    totalExtract -= 26;
+
+    if (totalExtract < 0) {
+
+        totalExtract = 0;
+
+    }
+
+
+
+    console.log(days);
+
+    console.log(totalExtract);
+
+}
+demo(450);
