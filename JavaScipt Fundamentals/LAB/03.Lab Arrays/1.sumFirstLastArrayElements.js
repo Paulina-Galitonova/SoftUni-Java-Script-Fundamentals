653f0a70acf651263a02e453442587de0cc85a3f
Paulina-Galitonova <145610504+Paulina-Galitonova@users.noreply.github.com>
2023-09-29 10:36:42 +0300

    01. Sum First and Last Array Elements compleated

---
+++ JavaScipt Fundamentals/LAB/03.Lab Arrays/1.sumFirstLastArrayElements.js
@@ -0,0 +1,18 @@
+function sumFirstLastArrayElements(array){
+
+    // Receives an array of numbers 
+    //Rrints the sum of the first and last element in that array.
+
+    let first = array[0];
+    let lastSymbol= array.length-1;
+    let last = array [array.length-1];
+
+    
+
+    console.log(first + last);
+
+   
+
+}
+
+sumFirstLastArrayElements([11, 58, 69]);
