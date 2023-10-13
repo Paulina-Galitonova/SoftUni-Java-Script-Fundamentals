d2041543454635fc8f063dd26e30d24ab701d49d
Paulina-Galitonova <145610504+Paulina-Galitonova@users.noreply.github.com>
2023-10-13 11:02:52 +0300

    compleated

--- JavaScipt Fundamentals/01. Programming Fundamentals Mid Exam Retake/02. The Lift.js
+++ JavaScipt Fundamentals/01. Programming Fundamentals Mid Exam Retake/02. The Lift.js
@@ -1,58 +1,42 @@
-function theLift(array) {
-    //Write a program that finds a place for the tourist on a lift. 
-    //Every wagon should have a maximum of 4 people on it. If a wagon is full, you should direct the people to the next one with space available.
-
-    // //•	On the first line, you will receive how many people are waiting to get on the lift
-    // •	On the second line, you will receive the current state of the lift separated by a single space: " ".
-
-    //When there is no more available space left on the lift, or there are no more people in the queue, you should print on the console the final state of the lift's wagons separated by " " and one of the following messages:
-
-   
-
-    let maxSpace=4
-
-
-    let people = Number(array[0]);
-    let listState = array[1];
-    let splitLift = listState.split(' ');
-
-
-
-    for (let i = 0; i < splitLift.length; i++) {
-        let wagon = Number(splitLift[i]);
-        let currentWagonSpace=maxSpace-wagon;
-        if (currentWagonSpace >=people){
-            splitLift[i] = people;
-            people=0;
-            break;
-
-        } else{
-            splitLift[i]=maxSpace;
-            people -= currentWagonSpace;
+function theLift(input) {
+    let peopleInQueue = Number(input.shift());
+    let wagons = input.toString().split(" ").map((x)=> Number(x));
+ 
+    for (let i = 0; i < wagons.length; i++) {
+        if (wagons[i] < 4) {
+            if (peopleInQueue >= 4 - wagons[i]) {
+                peopleInQueue -= 4 - wagons[i];
+                wagons[i] = 4;
+            } else {
+                wagons[i] += peopleInQueue;
+                peopleInQueue = 0;
+            }
         }
-
-       
     }
-
-  
-
-
- // •	If there are no more people and the lift has empty spots, you should print:
-    // "The lift has empty spots!
-    // {wagons separated by ' '}"
-    // •	If there are still people in the queue and no more available space, you should print:
-    // "There isn't enough space! {people} people in a queue!
-    // {wagons separated by ' '}"
-    // •	If the lift is full and there are no more people in the queue, you should print only the wagons separated by " ".
-
-
-
-
+ 
+    let isNotFull = false;
+ 
+    for (let i = 0; i < wagons.length; i++) {
+        let currenEl = wagons[i];
+ 
+        if (currenEl < 4) {
+            isNotFull = true;
+        }
+    }
+ 
+    if (!isNotFull && peopleInQueue > 0) {
+        console.log(`There isn't enough space! ${peopleInQueue} people in a queue!`); 
+    }
+ 
+    if (isNotFull && peopleInQueue == 0) {
+        console.log("The lift has empty spots!");
+    }
+ 
+    console.log(wagons.join(" "));
 }
-
 
 theLift([
     "15",
-    "0 0 0 0 0"
-]
-);
+    "0 2 0"
+   ]
+   );
