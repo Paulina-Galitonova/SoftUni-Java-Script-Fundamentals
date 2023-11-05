4ad62445ce94a9cc1c3233c4b4b3208af8a4938c
Paulina-Galitonova <145610504+Paulina-Galitonova@users.noreply.github.com>
2023-11-05 10:20:57 +0200

    change

--- JavaScipt Fundamentals/08.Objects and Classes/Exercise/07. Class Vehicle.js
+++ JavaScipt Fundamentals/08.Objects and Classes/Exercise/07. Class Vehicle.js
@@ -32,11 +32,11 @@ function classVehicle() {
 
     }
 
-    let parts = { engine: 6, power: 100 };
-    let vehicle = new Vehicle('a', 'b', parts, 200);
-    vehicle.drive(100);
+    let parts = {engine: 9, power: 500};
+    let vehicle = new Vehicle('l', 'k', parts, 840);
+    vehicle.drive(20);
     console.log(vehicle.fuel);
-    console.log(vehicle.parts.quality);
+    
 
 
 }
