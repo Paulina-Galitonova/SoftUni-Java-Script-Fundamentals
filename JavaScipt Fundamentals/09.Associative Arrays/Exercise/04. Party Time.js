526e62dd39843ef338f2b316ffada4400d568862
Paulina-Galitonova <145610504+Paulina-Galitonova@users.noreply.github.com>
2023-11-19 11:31:53 +0200

    Associative Arrays Exercise 04 Task compleated

--- JavaScipt Fundamentals/09.Associative Arrays/Exercise/04. Party Time.js
+++ JavaScipt Fundamentals/09.Associative Arrays/Exercise/04. Party Time.js
@@ -1,41 +1,24 @@
 function partyTime(input) {
-    let guests = input.slice(0, input.indexOf('PARTY'));
-    let attendees = input.slice(input.indexOf('PARTY') + 1);
-  
-    let vipGuests = [];
-    let regularGuests = [];
-  
-    guests.forEach((guest) => {
-      if (guest[0].match(/\d/)) {
-        vipGuests.push(guest);
+  let invitedList = input.splice(0, input.indexOf("PARTY"));
+  input.splice(0, 1);
+  let VIP = [];
+  let regular = [];
+  for (let guest of invitedList) {
+      if (guest[0] >= '0' && guest[0] <= '9') {
+          VIP.push(guest);
       } else {
-        regularGuests.push(guest);
+          regular.push(guest);
       }
-    });
-  
-    attendees.forEach((attendee) => {
-      if (attendee[0].match(/\d/)) {
-        vipGuests = vipGuests.filter((guest) => guest !== attendee);
-      } else {
-        regularGuests = regularGuests.filter((guest) => guest !== attendee);
-      }
-    });
-  
-    let allGuests = vipGuests.concat(regularGuests);
-  
-    console.log(allGuests.length);
-    console.log(allGuests.join('\n'));
   }
-  
-  // Example usage:
-  partyTime([
-    '7IK9Yo0h',
-    '9NoBUajQ',
-    'Ce8vwPmE',
-    'SVQXQCbc',
-    'tSzE5t0p',
-    'PARTY',
-    '9NoBUajQ',
-    'Ce8vwPmE',
-    'SVQXQCbc'
-  ]);
+  for (let guest of input) {
+      if (VIP.includes(guest)) {
+          VIP.splice(VIP.indexOf(guest), 1);
+      }
+      if (regular.includes(guest)) {
+          regular.splice(regular.indexOf(guest), 1);
+      }
+  }
+  console.log(VIP.length + regular.length);
+  VIP.forEach((x) => console.log(x));
+  regular.forEach((x) => console.log(x));
+}
